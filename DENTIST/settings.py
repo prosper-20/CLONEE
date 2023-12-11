f50ffbb9e89a32eeaf1bf2272df70cb637738331
prosper <edwardprosper001@gmail.com>
2023-12-11 20:52:33 +0100

    'updates'

--- DENTIST/settings.py
+++ DENTIST/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-production-31ce.up.railway.app']
+
+CSRF_TRUSTED_ORIGINS = ['https://web-production-31ce.up.railway.app']
 
 
 # Application definition
